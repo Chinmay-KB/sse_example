f0b4fd0408e22922fd95a8ee1e2dd74ec3f5f0d6
Chinmay <chinmay.kabi@live.com>
2022-04-16 11:35:14 +0530

    root returns text

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/status', (request, response) => response.json({clients: clients.length}));
+app.get('/status', (request, response) => response.json({ clients: clients.length }));
 
 const PORT = 3000;
 
@@ -17,10 +17,16 @@ let clients = [];
 let facts = [];
 
 app.listen(PORT, () => {
-  console.log(`Facts Events service listening at http://localhost:${PORT}`)
+    console.log(`Facts Events service listening at http://localhost:${PORT}`)
 });
 
 app.get('/events', eventsHandler);
+
+app.get('/', apiHome);
+
+function apiHome(request, response, next) {
+    response.send('Udao!');
+}
 
 // app.post('/fact', addFact);
 
@@ -29,49 +35,40 @@ app.get('/events', eventsHandler);
 
 function eventsHandler(request, response, next) {
     const headers = {
-      'Content-Type': 'text/event-stream',
-      'Connection': 'keep-alive',
-      'Cache-Control': 'no-cache'
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
     };
     response.writeHead(200, headers);
-  
+
     const data = `data: ${JSON.stringify(facts)}\n\n`;
-  
+
     response.write(data);
-  
+
     const clientId = Date.now();
-  
+
     const newClient = {
-      id: clientId,
-      response
+        id: clientId,
+        response
     };
-  
+
     clients.push(newClient);
-  
+
     request.on('close', () => {
-      console.log(`${clientId} Connection closed`);
-      clients = clients.filter(client => client.id !== clientId);
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(client => client.id !== clientId);
     });
-  }
+}
 
 
 function sendEventsToAll(message) {
     clients.forEach(client => client.response.write(`data: ${JSON.stringify(message)}\n\n`))
-  }
-let messageCount=0;
-setInterval(function (){
+}
+let messageCount = 0;
+setInterval(function() {
     sendEventsToAll({
-        "data":randomWords(),
-        "count":messageCount++,
-        "live_clients":clients.length
+        "data": randomWords(),
+        "count": messageCount++,
+        "live_clients": clients.length
     });
 }, 1000);
-  
-//   async function addFact(request, respsonse, next) {
-//     const newFact = request.body;
-//     facts.push(newFact);
-//     respsonse.json(newFact)
-//     return sendEventsToAll(newFact);
-//   }
-  
-  
